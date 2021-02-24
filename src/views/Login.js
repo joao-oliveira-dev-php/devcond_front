88da6222094b0af87dd75f831b4fe663f5a5d45b
João Oliveira <joaoo.rocha1@gmail.com>
2021-02-24 02:53:30 -0300

    Remoção de arquivos do core.ui desnecessario e configurar do navbar

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React , {useState}from 'react'
+import {useHistory} from 'react-router-dom';
 import {
+  CAlert,
   CButton,
   CCard,
   CCardBody,
@@ -16,25 +17,61 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
+import useApi from '../services/api';
+
 const Login = () => {
+  const api = useApi();
+  const history = useHistory();
+
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleLoginButton = async () => {
+    if(email && password) {
+      setLoading(true);
+
+      const result = await api.login(email, password);
+
+      setLoading(false);
+
+        if(result.error === '') {
+
+          localStorage.setItem('token', result.token);
+          history.push('/');
+
+        } else {
+          setError(result.error);
+        }
+
+    } else {
+      setError("Digite os dados");
+    }
+  }
   return (
     <div className="c-app c-default-layout flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md="8">
+          <CCol md="5">
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
                   <CForm>
                     <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
+                    <p className="text-muted">Digite seus dados de acesso</p>
+
+                    {error != '' &&
+                      <CAlert color="danger">{error}</CAlert>
+                    }
+
                     <CInputGroup className="mb-3">
                       <CInputGroupPrepend>
                         <CInputGroupText>
                           <CIcon name="cil-user" />
                         </CInputGroupText>
                       </CInputGroupPrepend>
-                      <CInput type="text" placeholder="Username" autoComplete="username" />
+                      <CInput type="text" placeholder="E-mail" disabled={loading} value={email} onChange={e=>setEmail(e.target.value)} />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupPrepend>
@@ -42,11 +79,11 @@ const Login = () => {
                           <CIcon name="cil-lock-locked" />
                         </CInputGroupText>
                       </CInputGroupPrepend>
-                      <CInput type="password" placeholder="Password" autoComplete="current-password" />
+                      <CInput type="password" placeholder="Senha" disabled={loading} value={password} onChange={e=>setPassword(e.target.value)} />
                     </CInputGroup>
                     <CRow>
                       <CCol xs="6">
-                        <CButton color="primary" className="px-4">Login</CButton>
+                        <CButton color="primary" className="px-4" onClick={handleLoginButton} disabled={loading}>{loading ? 'Carregando' : 'Entrar'}</CButton>
                       </CCol>
                       <CCol xs="6" className="text-right">
                         <CButton color="link" className="px-0">Forgot password?</CButton>
@@ -55,18 +92,7 @@ const Login = () => {
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                      labore et dolore magna aliqua.</p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
+              
             </CCardGroup>
           </CCol>
         </CRow>
